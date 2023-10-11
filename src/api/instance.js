f2bfb3345ca2fa343081993fbf2a2e8d8844da5f
Dasom Park <dev.somda@gmail.com>
2023-10-11 19:39:31 +0900

    🐛 fix: env key name

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,7 +1,7 @@
 import { Octokit } from "octokit";
 
-const KEY = process.env.GITHUB_ACCESS_KEY;
+const API_KEY = process.env.REACT_APP_GITHUB_ACCESS_KEY;
 
 export const octokitInstance = new Octokit({
-  auth: KEY,
+  auth: API_KEY,
 });
